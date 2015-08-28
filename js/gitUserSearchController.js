7d166f21a6c1fae41ebcdba303d1cfba5255ed2d
andygout <andygout@hotmail.co.uk>
2015-08-28 12:10:13 +0100

    Prevent: Empty text field attempting search

--- js/gitUserSearchController.js
+++ js/gitUserSearchController.js
@@ -2,9 +2,12 @@ githubUserSearch.controller('GitUserSearchController', ['Search', function(Searc
   var self = this;
 
   self.doSearch = function() {
-    Search.query(self.searchTerm)
-      .then(function(response) {
+    self.searchResult = []
+    if(self.searchTerm) {
+      Search.query(self.searchTerm)
+        .then(function(response) {
         self.searchResult = response.data;
-      })
+      });
+    }
   };
 }]);
