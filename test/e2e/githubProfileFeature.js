fc63eed6080cb60ea6fbbda61d02e12eab96e0c3
Andy Gout <andygout@users.noreply.github.com>
2017-07-13 21:20:26 +0100

    Comment out: Tests reliant on third party data (#2)

--- test/e2e/githubProfileFeature.js
+++ test/e2e/githubProfileFeature.js
@@ -17,18 +17,18 @@ describe('GitHub profile finder', function() {
     expect(element(by.binding('user.login')).getText()).toEqual('andygout');
   });
 
-  it('finds last profile of results', function() {
-    searchBox.sendKeys('andygou');
-    searchButton.click();
-    var profiles = element.all(by.repeater('user in searchCtrl.searchResult'));
-    expect(profiles.last().getText()).toContain('andygould');
-  });
+  // it('finds last profile of results', function() {
+  //   searchBox.sendKeys('andygou');
+  //   searchButton.click();
+  //   var profiles = element.all(by.repeater('user in searchCtrl.searchResult'));
+  //   expect(profiles.last().getText()).toContain('andygould');
+  // });
 
-  it('counts the number of results', function() {
-    searchBox.sendKeys('andygou');
-    searchButton.click();
-    var profiles = element.all(by.repeater('user in searchCtrl.searchResult'));
-    expect(profiles.count()).toEqual(5);
-  });
+  // it('counts the number of results', function() {
+  //   searchBox.sendKeys('andygou');
+  //   searchButton.click();
+  //   var profiles = element.all(by.repeater('user in searchCtrl.searchResult'));
+  //   expect(profiles.count()).toEqual(5);
+  // });
 
 });
